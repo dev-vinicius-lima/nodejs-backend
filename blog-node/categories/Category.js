23e480b0407aec6481e76becbdb6895f7bc9a366
dev-vinicius-lima <viniciuslimaes@hotmail.com>
2024-07-15 11:31:20 -0400

    feat: :sparkles: created model category

---
+++ blog-node/categories/Category.js
@@ -0,0 +1,16 @@
+import Sequelize from "sequelize";
+import connection from "../database/database.js";
+
+const Category = connection.define("categories", {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  slug: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+})
+
+
+export default Category;
