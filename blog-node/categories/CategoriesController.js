e2aad7bfae560b25403f5766e1d08215e0f1dc4e
dev-vinicius-lima <viniciuslimaes@hotmail.com>
2024-07-15 17:22:16 -0400

    feat: :sparkles: create new categorie

--- blog-node/categories/CategoriesController.js
+++ blog-node/categories/CategoriesController.js
@@ -1,12 +1,32 @@
 import express from "express";
+import Category from "./Category.js";
+import slugify from "slugify";
 const router = express.Router();
 
-router.get("/categories", (req, res) => {
-  res.send("rota de Categories")
+router.get("/admin/categories/new", (req, res) => {
+  res.render("admin/categories/new")
 })
 
-router.get("/admin/categories/new", (req, res) => {
-  res.send("rota para criar uma nova Categories")
+router.post("/categories/save", (req, res) => {
+  const { title } = req.body
+  if (title !== undefined) {
+    Category.count({ where: { title: title } }).then(count => {
+      if (count == 0) {
+        Category.create({ title, slug: slugify(title) }).then(() => {
+          res.redirect("/admin/categories/index")
+        })
+      }
+      else {
+        res.redirect("/admin/categories/new")
+      }
+    })
+  }
+})
+
+router.get("/admin/categories", (req, res) => {
+  Category.findAll().then(categories => {
+    res.render("admin/categories/index", { categories: categories })
+  })
 })
 
 
