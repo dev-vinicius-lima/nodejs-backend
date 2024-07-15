d44c30ebaf64c5ee852029988b3d1bafa09a5a6f
dev-vinicius-lima <viniciuslimaes@hotmail.com>
2024-07-15 11:30:25 -0400

    feat: :sparkles: created model article

---
+++ blog-node/articles/Article.js
@@ -0,0 +1,26 @@
+import Sequelize from "sequelize";
+import connection from "../database/database.js";
+import Category from '../categories/Category.js';
+
+const Article = connection.define("article", {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  slug: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  body: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  }
+})
+
+Category.hasMany(Article); // 1 p m
+Article.belongsTo(Category); // 1 p 1
+
+Article.sync({ force: true });
+
+
+export default Article;
